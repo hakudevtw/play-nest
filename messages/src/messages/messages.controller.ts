bf6ba2ce6f15fb3fe7806a321289a9a0a3a14f07
Haku <hakudevtw@gmail.com>
2024-09-19 17:03:52 +0800

    feat: add in routes

--- messages/src/messages/messages.controller.ts
+++ messages/src/messages/messages.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
+// messages/* is the root path for all routes in this controller
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  @Get()
+  getMessages() {
+    return 'All messages';
+  }
+
+  @Get(':id')
+  getMessage() {
+    return 'One message';
+  }
+
+  @Post()
+  createMessage() {
+    return 'Message added';
+  }
+}
