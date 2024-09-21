cd5854ef298a5279c1d0cee77b620db9dcd492da
Haku <hakudevtw@gmail.com>
2024-09-21 12:00:34 +0800

    feat: add needed service and controllers

--- apps/mycv/src/app.module.ts
+++ apps/mycv/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ReportsController } from './reports/reports.controller';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
+import { ReportsService } from './reports/reports.service';
+import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule, UsersModule, ReportsModule],
+  controllers: [AppController, ReportsController, UsersController],
+  providers: [AppService, UsersService, ReportsService],
 })
 export class AppModule {}
