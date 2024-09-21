79ab737b877db3b24f1fd303c19e271d902b803b
Haku <hakudevtw@gmail.com>
2024-09-21 14:51:38 +0800

    feat: implement users controller

--- apps/mycv/src/users/users.controller.ts
+++ apps/mycv/src/users/users.controller.ts
@@ -1,5 +1,15 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Patch,
+  Delete,
+  Param,
+  Query,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/creata-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
@@ -10,5 +20,25 @@ export class UsersController {
   createUser(@Body() body: CreateUserDto) {
     const { email, password } = body;
     return this.usersService.create(email, password);
+  }
+
+  @Get(':id')
+  findUser(@Param('id') id: string) {
+    return this.usersService.findOne(parseInt(id));
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Patch(':id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
+  }
+
+  @Delete(':id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
   }
 }
