a87a1e6c6c02141da79e8b9bb1636573ef809c6d
Haku <hakudevtw@gmail.com>
2024-09-21 10:00:50 +0800

    feat: connect power service with cpu service

--- di/src/cpu/cpu.module.ts
+++ di/src/cpu/cpu.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CpuService } from './cpu.service';
+import { PowerModule } from '../power/power.module';
 
 @Module({
+  // Import to use services exported from PowerModule
+  imports: [PowerModule],
   providers: [CpuService],
 })
 export class CpuModule {}
--- di/src/power/power.module.ts
+++ di/src/power/power.module.ts
@@ -3,5 +3,7 @@ import { PowerService } from './power.service';
 
 @Module({
   providers: [PowerService],
+  // Export PowerService to make it available to other modules
+  exports: [PowerService],
 })
 export class PowerModule {}
