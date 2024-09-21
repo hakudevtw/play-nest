b6574bb24d60e3f9bbf1dcb33662389150f79a89
Haku <hakudevtw@gmail.com>
2024-09-21 14:29:05 +0800

    feat: implement user service

--- apps/mycv/src/users/users.service.ts
+++ apps/mycv/src/users/users.service.ts
@@ -21,4 +21,32 @@ export class UsersService {
     // Also lose the ability to validate the entity
     // return this.repo.save({ email, password });
   }
+
+  findOne(id: number) {
+    return this.repo.findOneBy({ id });
+  }
+
+  find(email: string) {
+    return this.repo.find({ where: { email } });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    // insert and update will not run hooks in the entity
+    // return this.repo.update(id, attrs);
+
+    // Fetch the user entity first (not so efficient)
+    const user = await this.findOne(id);
+    if (!user) throw new Error('User not found');
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    // delete will not run hooks in the entity
+    // return this.repo.delete(id);
+
+    const user = await this.findOne(id); // User entity
+    if (!user) throw new Error('User not found');
+    return this.repo.remove(user);
+  }
 }
