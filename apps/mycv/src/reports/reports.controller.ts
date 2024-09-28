ec5ff9a99108583544b40353e5bec0c77357d5b9
Haku <hakudevtw@gmail.com>
2024-09-28 17:05:20 +0800

    feat: create estimate

--- apps/mycv/src/reports/reports.controller.ts
+++ apps/mycv/src/reports/reports.controller.ts
@@ -5,9 +5,12 @@ import {
   UseGuards,
   Patch,
   Param,
+  Get,
+  Query,
 } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportDto } from './dtos/report.dto';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from '../guards/auth.guard';
 import { AdminGuard } from '../guards/admin.guard';
@@ -32,5 +35,10 @@ export class ReportsController {
   approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
     const { approved } = body ?? {};
     return this.reportsService.changeApproval(id, approved);
+  }
+
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {
+    return this.reportsService.createEstimate(query);
   }
 }
